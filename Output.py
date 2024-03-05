07b9e94d7f351ffd51b997ef0d05fa5605e90477
Meljames Cortes <157451950+Nikolajmc@users.noreply.github.com>
2024-03-05 15:36:05 +0800

    first commit

---
+++ Output.py
@@ -0,0 +1,16 @@
+# VARIABLE DECLARATION
+
+x = 25
+message = "Hi oi"
+ratio = 3.14
+
+# CHECKING THE TYPE OF DATA WITH TYPE() METHOD
+
+print(type(message))
+print(type(ratio))
+print(type(x))
+
+# STRING FORMATTING F FOR FSTRING
+
+print(message + " My name is Pappy Bords. I am " + str(x) + " years old.")
+print(f"{message} My name is Pappy Bords. I am {x} years old.")
